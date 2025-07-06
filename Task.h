879d209628db5996de28c00ac9fd5f1a3c0a5aad
Andrew Qin <andrewqin29@gmail.com>
2025-07-05 18:41:47 -0700

    header file for task class

---
+++ Task.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <chrono>
+
+enum class Priority {
+	Low,
+	Med,
+	High
+};
+
+class Task {
+	private:
+		std::string description;
+		bool is_complete;
+		Priority priority;
+		
+	public:
+		// Constructor
+		Task(const std::string& description, Priority priority);
+
+		// Getters -- retrieving task fields
+		std::string get_description() const;
+		bool is_complete() const;
+		Priority get_priority() const;
+		
+		// Setters -- modifying task states
+		void mark_is_complete(bool status);
+}
