ff13c108fa01d1f90951195f7f70c85441ef3073
Andrew Qin <andrewqin29@gmail.com>
2025-07-05 23:59:16 -0700

    command handlers in main

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ void save_tasks_to_file(const std::vector<Task>& tasks);
 std::vector<Task> load_tasks_from_file();
 void display_tasks(const std::vector<Task>& tasks);
 void display_help();
+void display_command_err();
 
 // ------- MAIN -------
 int main(int argc, char* argv[]) {
@@ -49,7 +50,115 @@ int main(int argc, char* argv[]) {
 	std::vector<Task> tasks = load_tasks_from_file();
 
 	// **** COMMAND HANDLING ****
+	if (argc < 2) { // 'todo'
+		display_tasks(tasks);
+		return 0
+	}
 
+	std::string command = argv[1];
+
+	if (command == "add") {
+		if (argc < 3) {
+            std::cout << "Error: Missing task description." << std::endl;
+            std::cout << "Usage: todo add \"<description>\" [--priority <level>]" << std::endl;
+            return 1;
+        }
+
+        std::string description = argv[2];
+        Priority priority = Priority::Low;
+
+        for (int i = 3; i < argc; ++i) {
+            std::string arg = argv[i];
+            if (arg == "--priority" && i + 1 < argc) {
+                std::string p_level = argv[i + 1];
+                if (p_level == "high") {
+                    priority = Priority::High;
+                } else if (p_level == "med") {
+                    priority = Priority::Med;
+                } else if (p_level == "low") {
+                    priority = Priority::Low;
+                }
+                
+                // we've processed the flag and its value, so skip the next argument
+                i++; 
+            }
+        }
+	} else if (command == "display") {
+		if (argc == 2) {
+			display_tasks(tasks);
+		} else {
+			std::string flag = argv[2];
+			if (flag == "priority") {
+				// sort tasks based on priority (high > med > low)
+				std::sort(tasks.begin(), tasks.end(), [](const Task& a, const Task& b) {
+					return static_cast<int>(a.get_priority()) > static_cast<int>(b.get_priority());
+				});
+				display_tasks(tasks);
+			} else if (flag == "pending") {
+				// create new tasks vector only containing pending tasks
+				std::vector<Task> pending_tasks;
+				for (const Task &task : tasks) {
+					if (!task.is_complete) {
+						pending_tasks.push_back(task);
+					}
+				}
+				display_tasks(pending_tasks);
+			} else {
+				display_command_err();
+			}
+		}
+		return 0;
+		
+	} else if (command == "done" || command == "undone") {
+		if (argc < 3) {
+            std::cout << "Error: Please provide a task index." << std::endl;
+            return 1;
+        }
+        try {
+            int index = std::stoi(argv[2]);
+            if (index <= 0 || static_cast<size_t>(index) > tasks.size()) {
+                std:: cout << "Error: index out of bounds." << std::endl;
+                return 1;
+            }
+
+            Task& task_to_modify = tasks.at(index - 1);
+
+            if (command == "done") {
+                task_to_modify.mark_is_complete(true);
+                std::cout << "Task " << index << " marked as complete." << std::endl;
+            } else { // command == "undone"
+                task_to_modify.mark_is_complete(false);
+                std::cout << "Task " << index << " marked as pending." << std::endl;
+            }
+        } catch (const std::invalid_argument& e) {
+            std::cout << "Error: Invalid index. Please provide a number." << std::endl;
+        } catch (const std::out_of_range& e) {
+            std::cout << "Error: The index you entered is too large." << std::endl;
+        }
+        
+	} else if (command == "remove") {
+		if (argc == 3) {
+			try {
+				index = std::stoi(argv[2]);
+				if (index <= 0 || static_cast<size_t>(index) > tasks.size()) {
+				    std:: cout << "Error: index out of bounds." << std::endl;
+				    return 1; 
+				}
+				tasks.erase(tasks.begin() + index - 1);
+				std::cout << "Task at index " << argv[2] << " removed." << std::endl;
+			} catch (...) {
+				std::cout << "Error: invalid index specifier." << std::endl;
+			}
+		} else {
+			display_command_err();
+		}
+
+	} else if (command == "help") {
+		display_help();
+		return 0;
+	} else {
+		display_command_err();
+	}
 	
 	return 0;
 }
@@ -58,7 +167,7 @@ int main(int argc, char* argv[]) {
 std::filesystem::path get_task_path() {
 	const char *home_dir = getenv("HOME")
 	if (home_dir == nullptr) {
-		std::cerr << "Error. Could not locate home directory." << std::endl;
+		std::cerr << "Error: Could not locate home directory." << std::endl;
 		return;
 	}
 	
@@ -74,7 +183,7 @@ void save_tasks_to_file(const std::vector<Task>& tasks) {
 	ofs.open(tasks_path, std::ofstream::out | std::ofstream::trunc); // erase all
 
 	if (!ofs.is_open()) {
-		std::cerr << "Error. Could not open tasks file." << std::endl;
+		std::cerr << "Error: Could not open tasks file." << std::endl;
 		return;
 	}
 	
@@ -198,4 +307,9 @@ void display_help() {
     std::cout << "    Permanently removes the task at the given index." << std::endl << std::endl;
     std::cout << "  help" << std::endl;
     std::cout << "    Shows this help message." << std::endl;
+}
+
+void display_command_err() {
+	std::cout << "Error: Unknown command '" << command << "'" << std::endl;
+	std::cout << "Run 'todo help' to see a list of available commands." << std::endl;
 }
